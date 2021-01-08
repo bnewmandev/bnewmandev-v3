58df46f6b74fd7411f531f99d97a713e54763fde
Ben Newman <ben@bnewman.dev>
2021-01-08 20:14:19 +0000

    init

---
+++ front-end/bnewmandotdev/src/Components/HomeIntroduction.jsx
@@ -0,0 +1,18 @@
+const HomeIntro = () => {
+	return (
+		<div id="home-intro">
+			<main>
+				<h2>Lorem ipsum dolor sit amet.</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui,
+					voluptates fuga aperiam atque sunt modi reprehenderit obcaecati
+					exercitationem similique nemo, at repudiandae quis veniam omnis natus
+					eveniet aliquam, quasi a?
+				</p>
+				<button>Learn More</button>
+				<button>View CV</button>
+			</main>
+		</div>
+	);
+};
+export default HomeIntro;
---
+++ front-end/bnewmandotdev/src/Components/MySkills.jsx
@@ -0,0 +1,18 @@
+const MySkills = () => {
+	return (
+		<header>
+			<div id="heading">
+				<img src="https://bnewman.dev/assets/logo.png" alt="BNewman.dev" />
+				<h1>BNewman.Dev</h1>
+				<button id="home">Home</button>
+				<button id="myPlans">My Plans</button>
+				<button id="aboutMe">About Me</button>
+				<button id="myProjects">My Projects</button>
+				<button id="links">Links</button>
+				<button id="contactMe">Contact Me</button>
+			</div>
+		</header>
+	);
+};
+
+export default MySkills;
