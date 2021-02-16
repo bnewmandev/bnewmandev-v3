71739bb5fbd108a6839e6eabd164c03debb47039
Ben Newman <ben@bnewman.dev>
2021-02-16 17:01:45 +0000

    update

--- front-end/bnewmandotdev/src/Components/HomeMySkills.jsx
+++ front-end/bnewmandotdev/src/Components/HomeMySkills.jsx
@@ -2,18 +2,11 @@ import HomeSkill from "./HomeSkill";
 import data from "../data.json";
 const HomeMySkills = () => {
 	const skills = data.skills;
-	const learning = data.learning;
 	return (
 		<div id="HomeMySkills">
 			<h1>My Skills</h1>
 			<div className="HomeMySkillsWrapper">
 				{skills.map((element) => (
-					<HomeSkill element={element} key={element.id} />
-				))}
-			</div>
-			<h1>Currently Learning</h1>
-			<div className="HomeMySkillsWrapper">
-				{learning.map((element) => (
 					<HomeSkill element={element} key={element.id} />
 				))}
 			</div>
--- front-end/bnewmandotdev/src/Components/HomeSkill.jsx
+++ front-end/bnewmandotdev/src/Components/HomeSkill.jsx
@@ -1,11 +1,23 @@
 const HomeSkill = (element) => {
 	console.log(element);
+	const toggleDisplay = (e) => {
+		let target = e.target;
+	};
 	return (
-		<a href={element.element.link} id={element.element.id}>
+		<button id={element.element.id} onClick={toggleDisplay}>
 			<h2>{element.element.title}</h2>
 			<img src={element.element.image} alt={element.element.title} />
 			<p>{element.element.body}</p>
-		</a>
+			<div id={element.element.id + "OVERLAY"} className="MINOR-OVERLAY">
+				{console.log(element.element.id + "OVERLAY")}
+				{element.element.eg.map((element) => (
+					<div>
+						<h1>{element.heading}</h1>
+						<p>{element.body}</p>
+					</div>
+				))}
+			</div>
+		</button>
 	);
 };
 export default HomeSkill;
