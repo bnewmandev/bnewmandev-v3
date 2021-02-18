0378a1bbdb0aaba467ee7f736fc08ecf8617e519
Ben Newman <ben@bnewman.dev>
2021-02-18 11:19:47 +0000

    updates

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const path = require("path");
-const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
+const sgMail = require("@sendgrid/mail");
+const fs = require("fs");
 require("dotenv").config();
 
+const storeData = (data, path) => {
+	try {
+		fs.writeFileSync(path, JSON.stringify(data));
+	} catch (err) {
+		console.error(err);
+	}
+};
+
 const app = express();
+sgMail.setApiKey(process.env.GRIDKEY);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "build")));
@@ -14,17 +24,8 @@ app.get("*", function (req, res) {
 });
 
 app.post("/contactme", (req, res) => {
-	const smtpTrans = nodemailer.createTransport({
-		host: "smtp.gmail.com",
-		port: 465,
-		secure: true,
-		auth: {
-			user: process.env.EMAIL,
-			pass: process.env.PASS,
-		},
-	});
 	const mailOpts = {
-		from: "Ben Newman Dev Contact Form",
+		from: process.env.EMAIL,
 		to: process.env.EMAIL,
 		subject: `New Mesage from ${req.body.name} at bnewman.dev`,
 		text: `
@@ -34,13 +35,17 @@ app.post("/contactme", (req, res) => {
 		Msg: ${req.body.msg}
 		`,
 	};
-	smtpTrans.sendMail(mailOpts, (error, response) => {
-		if (error) {
+	sgMail
+		.send(mailOpts)
+		.then(() => {
+			console.log("Email sent");
+			res.redirect("https://bnewman.dev/contactme?success=true");
+		})
+		.catch((error) => {
 			console.error(error);
-		} else {
-			console.log("Message Sent");
-		}
-	});
+			storeData(error, "log.json");
+			res.redirect("https://bnewman.dev/contactme?success=false");
+		});
 });
 
 app.listen(3000);
