ad32477dec162d14dd2d3b80341826a07737ee5c
Ben Newman <ben@bnewman.dev>
2021-02-18 00:14:18 +0000

    fix spelling

--- front-end/bnewmandotdev/src/Views/ContactMe.jsx
+++ front-end/bnewmandotdev/src/Views/ContactMe.jsx
@@ -16,10 +16,12 @@ const ContactMe = () => {
 			<main>
 				<h1 className="MAINHEADING">Contact Me</h1>
 				<h2>
-					For any buisness enquiries or any other feedback or questions please
+					For any business enquiries or any other feedback or questions please
 					contact me at (email is preferred):
 				</h2>
-				<h3>Email: ben@bnewman.dev</h3>
+				<h3>
+					<a href="mailto:ben@bnewman.dev">Email: ben@bnewman.dev</a>
+				</h3>
 				<h3>Phone: +44 7860 241605</h3>
 			</main>
 			<Footer />
